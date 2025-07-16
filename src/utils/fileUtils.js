6fe737af23b7222670c4478ac9008af29db544a6
Meetbhesara <meetbhesara26@gmail.com>
2025-07-16 11:09:23 +0530

    Add fileUtils utility for reading and writing JSON data

---
+++ src/utils/fileUtils.js
@@ -0,0 +1,17 @@
+const fs =require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '../../data/sweets.json');
+
+function readData() {
+    return Json.parse(fs.readFileSync(filePath, 'utf-8'));
+}
+
+function writeData(data) {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+}
+
+module.exports = {
+    readData,
+    writeData
+};
