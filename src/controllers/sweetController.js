3659fe968d2425c798430636516a7eb2d9b24d2e
Meetbhesara <meetbhesara26@gmail.com>
2025-07-16 12:06:54 +0530

    add feature of view / see the data of sweets.json file

--- src/controllers/sweetController.js
+++ src/controllers/sweetController.js
@@ -2,11 +2,18 @@ const { readData , writeData } =require('../utils/fileUtils');
 
 function addSweet(sweet){
     const sweets = readData();
-    sweets.push(sweet);
     sweet.id = sweets.length ? sweets[sweets.length - 1].id + 1 : 1; // Assign an ID based on the current length
+    sweets.push(sweet); // Add the new sweet to the array
     writeData(sweets);
     return sweet;
 }
 
+function viewAllSweets()
+{
+    return readData(); // Read and return all sweets from the file
+}
+
 module.exports = {
-    addSweet };
+    addSweet,
+    viewAllSweets
+ };
