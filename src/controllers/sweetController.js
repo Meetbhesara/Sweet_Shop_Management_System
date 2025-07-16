bbaa553b39c1dc3dca9a544e5bc6065a48a466cc
Meetbhesara <meetbhesara26@gmail.com>
2025-07-16 12:49:03 +0530

    add feature of delete By Id

--- src/controllers/sweetController.js
+++ src/controllers/sweetController.js
@@ -13,7 +13,23 @@ function viewAllSweets()
     return readData(); // Read and return all sweets from the file
 }
 
+
+// delete the sweets
+
+function deleteSweetById(id) {
+    const sweets=readData();
+    const index= sweets.findIndex(sweet=> sweet.id=== id);
+    if(index===-1)
+    throw new Error("Sweet not found");
+    
+    const deletedSweet = sweets.splice(index, 1)[0]; // Remove the sweet from the array
+    writeData(sweets); // Write the updated array back to the file
+    return deletedSweet; // Return the deleted sweet
+}
+
+
 module.exports = {
     addSweet,
-    viewAllSweets
+    viewAllSweets,
+    deleteSweetById
  };
